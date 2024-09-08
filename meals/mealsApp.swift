bc24ebee4a434e3914b5b98f32a7667d18f0ffac
Raul Mena <rmena@Rauls-MacBook-Pro.local>
2024-09-08 16:32:00 -0400

    Create Xcode project

---
+++ meals/mealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealsApp.swift
+//  meals
+//
+//  Created by Raul Mena on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct mealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
